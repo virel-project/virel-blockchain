af9596e26806bf749d494335a74b4fd7f051840b
josetornante <209896672+josetornante@users.noreply.github.com>
2025-06-10 13:19:33 -0400

    prepare stagenet seed node

--- config/config_mainnet.go
+++ config/config_mainnet.go
@@ -2,9 +2,9 @@
 
 package config
 
-const P2P_BIND_PORT = 6310
-const RPC_BIND_PORT = 6311
-const STRATUM_BIND_PORT = 6312
+const P2P_BIND_PORT = 6313
+const RPC_BIND_PORT = 6314
+const STRATUM_BIND_PORT = 6315
 const NETWORK_ID uint64 = 0x4af15cf1542ba49a // Network identifier. It MUST be unique for each chain
 
 const NETWORK_NAME = "stagenet"
@@ -14,4 +14,4 @@ const GENESIS_ADDRESS = "so3yexhnu89af4aai83uou17dupb79c3gxng1q"
 const GENESIS_TIMESTAMP = 0
 const BLOCK_REWARD_FEE_PERCENT = 10
 
-var SEED_NODES = []string{"127.0.0.1:6310"}
+var SEED_NODES = []string{"82.153.138.24:6313"}
