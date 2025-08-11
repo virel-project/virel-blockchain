3159d310188f240dbab1a858fa3123ceb7effc0d
josetornante <209896672+josetornante@users.noreply.github.com>
2025-08-11 16:24:14 +0200

    fix wallet rpc client for ip:port

--- rpc/walletrpc/rpcclient.go
+++ rpc/walletrpc/rpcclient.go
@@ -7,6 +7,7 @@ import (
 	"errors"
 	"io"
 	"net/http"
+	"strings"
 
 	"github.com/virel-project/virel-blockchain/rpc"
 )
@@ -17,6 +18,9 @@ type RpcClient struct {
 }
 
 func NewRpcClient(addr, basicauth string) *RpcClient {
+	if !strings.Contains(addr, "://") {
+		addr = "http://" + addr
+	}
 	return &RpcClient{
 		WalletRpcAddress: addr,
 		BasicAuth:        basicauth,
