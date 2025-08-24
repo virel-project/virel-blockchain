548318c38c43a8b11933400e848807d54e2e1c6a
josetornante <209896672+josetornante@users.noreply.github.com>
2025-08-24 11:55:04 +0200

    version 1.2.3

--- config/config_testnet.go
+++ config/config_testnet.go
@@ -12,7 +12,7 @@ const NETWORK_NAME = "testnet"
 const MIN_DIFFICULTY = 1000
 
 // GENESIS BLOCK INFO
-const GENESIS_ADDRESS = "so3yexhnu89af4aai83uou17dupb79c3gxng1q"
+const GENESIS_ADDRESS = "vo3yexhnu89af4aai83uou17dupb79c3gxng1q"
 const GENESIS_TIMESTAMP = 0
 const BLOCK_REWARD_FEE_PERCENT = 10
 
