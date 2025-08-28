50023a193edbf24f14668aec68a04da97f6fa2db
josetornante <209896672+josetornante@users.noreply.github.com>
2025-08-28 19:54:33 +0200

    update state data

--- blockchain/state.go
+++ blockchain/state.go
@@ -9,9 +9,11 @@ import (
 )
 
 type State struct {
-	Balance      uint64
-	LastNonce    uint64
-	LastIncoming uint64 // not used in consensus, but we store it to list the wallet incoming transactions
+	Balance        uint64
+	LastNonce      uint64
+	LastIncoming   uint64 // not used in consensus, but we store it to list the wallet incoming transactions
+	DelegateId     uint64
+	DelegateAmount uint64
 }
 
 func (x State) Serialize() []byte {
@@ -20,6 +22,9 @@ func (x State) Serialize() []byte {
 	s.AddUvarint(x.Balance)
 	s.AddUvarint(x.LastNonce)
 	s.AddUvarint(x.LastIncoming)
+	s.AddUint8(1)
+	s.AddUvarint(x.DelegateId)
+	s.AddUvarint(x.DelegateAmount)
 
 	return s.Output()
 }
@@ -30,6 +35,15 @@ func (x *State) Deserialize(d []byte) error {
 	x.Balance = s.ReadUvarint()
 	x.LastNonce = s.ReadUvarint()
 	x.LastIncoming = s.ReadUvarint()
+
+	if len(s.RemainingData()) > 0 {
+		v := s.ReadUint8()
+		if v != 1 {
+			return errors.New("invalid state version")
+		}
+		x.DelegateId = s.ReadUvarint()
+		x.DelegateAmount = s.ReadUvarint()
+	}
 
 	return s.Error()
 }
